c820861b0e46d3874b3d12f1c29744571c3acb29
ZHB <ZHB@ZHB-PC>
2017-07-17 01:39:30 +0800

    完善

--- src/view/StockDetail.js
+++ src/view/StockDetail.js
@@ -29,9 +29,9 @@ class StockDetail extends Component {
             this.setState({
                 action: 'update'
             });
+            this.handleLoadStock();
         }
 
-        this.handleLoadStock();
         this.handleLoadProduct();
     }
 
