83d17fd076803d4c62df65379b58204763226f8b
marcus <marcus_hualebin@163.com>
2017-09-30 16:20:29 +0800

    完善

--- src/util/济颐馆constant.js
+++ src/util/济颐馆constant.js
@@ -36,27 +36,27 @@ export default {
         category_id: '0',
         category_name: '所有商品',
         category_color: '#fd666b',
-        category_image: 'apps.svg',
+        category_image: 'apps.svg'
     }, {
         category_id: '146474b15ba545d9b9717cf8b5a6c3f5',
         category_name: '肠内营养',
         category_color: '#73b4ef',
-        category_image: 'discover.svg',
+        category_image: 'discover.svg'
     }, {
         category_id: '9ed6cb3551fb4bfaabfeee89cc63f9b4',
         category_name: '快康系列',
         category_color: '#e78ab0',
-        category_image: 'ticket.svg',
+        category_image: 'ticket.svg'
     }, {
         category_id: '34fb354194e0409e8a80a4382a7fa18d',
-        category_name: '特殊奶粉',
+        category_name: '妇儿专区',
         category_color: '#7acfa6',
-        category_image: 'present.svg',
+        category_image: 'present.svg'
     }, {
         category_id: '26ef74aa1bb242479df5305478f31b08',
         category_name: '理疗辅助',
         category_color: '#ffcb63',
-        category_image: 'punch.svg',
+        category_image: 'punch.svg'
     }, {
         category_id: '45ac41e5c3334439a6ac45abdea31a30',
         category_name: '补血系列',
